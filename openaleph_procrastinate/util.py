65d21c893bb88706034fe0dc97bb781f61202c67
Alex Ștefănescu <alex.stefanescu@pm.me>
2026-04-01 20:46:55 +0200

    Implement get_page_entity_fragments

--- openaleph_procrastinate/util.py
+++ openaleph_procrastinate/util.py
@@ -2,9 +2,24 @@ from typing import Type
 
 from anystore.logging import get_logger
 from followthemoney import E, ValueEntity
+from followthemoney.proxy import EntityProxy
+from followthemoney.util import make_entity_id
+from followthemoney.namespace import Namespace
 from ftmq.util import make_entity
+from ftmq.store.fragments import get_fragments
+
+from openaleph_procrastinate.settings import OpenAlephSettings
 
 log = get_logger(__name__)
+
+# FTMQ BulkLoader default size = 1000
+QUERY_LIMIT = 1000
+
+openaleph_settings = OpenAlephSettings()
+sqlalchemy_pool = {
+    "pool_size": openaleph_settings.db_pool_size,
+    "max_overflow": openaleph_settings.db_pool_size,
+}
 
 
 def make_stub_entity(e: E, entity_type: Type[E] | None = ValueEntity) -> E:
@@ -31,3 +46,39 @@ def make_file_entity(
         stub.add("fileName", e.get("fileName", quiet=q), quiet=q)
         stub.add("parent", e.get("parent", quiet=q), quiet=q)
         return stub
+
+
+def get_page_entity_fragments(
+    entity: EntityProxy, ftm_dataset: str, ns: Namespace, origin: str = "ingest"
+):
+    """
+    Get all the Page entities corresponding to a Pages entity
+    """
+    store = get_fragments(
+        ftm_dataset,
+        origin=origin,
+        database_uri=openaleph_settings.fragments_uri,
+        **sqlalchemy_pool,
+    )
+    current_page = 1
+    while True:
+        page_batch = range(current_page, current_page + QUERY_LIMIT)
+        page_ids = set(
+            make_entity_id(entity.id, p, key_prefix=ftm_dataset) for p in page_batch
+        )
+        # https://github.com/openaleph/ingest-file/issues/30
+        page_id_no_ns = set(
+            make_entity_id(entity.id.split(".")[0], p, key_prefix=ftm_dataset)
+            for p in page_batch
+        )
+        page_ids.update(page_id_no_ns)
+        # apply correct namespace
+        page_ids = list(map(ns.sign, page_ids))
+        ix = 1
+        for ix, fragment in enumerate(  # noqa: B007
+            store.fragments(page_ids, "default"), 1
+        ):
+            yield fragment
+        if ix <= QUERY_LIMIT:
+            break
+        current_page += QUERY_LIMIT
